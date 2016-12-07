32fa70bde94aea0a4fde4670840d095ec6cd5d7e
pongsakon <Luzezeir@gmail.com>
2016-12-07 11:25:40 +0700

    first commit

---
+++ app/src/main/java/com/pongsakon/pongsakon_restaurant/FoodTABLE.java
@@ -0,0 +1,39 @@
+package com.pongsakon.pongsakon_restaurant;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Admin on 19-Oct-16.
+ */
+public class FoodTABLE {
+    //Explicit
+
+    private MySQLiteOpenHelper objMySQLiteOpenHelper;
+
+    private SQLiteDatabase writeSqLiteDatabase;
+    private SQLiteDatabase readSqLiteDatabase;
+
+    public static final String FOOD_TABLE = "foodTABLE";
+    public static final String COLUMN_IN_FOOD = "_id";
+    public static final String COLUMN_FOOD = "Food";
+    public static final String COLUMN_SOURCE = "Source";
+    public static final String COLUMN_PRICE = "Price";
+
+    public FoodTABLE(Context context) {
+        objMySQLiteOpenHelper = new MySQLiteOpenHelper(context);
+        writeSqLiteDatabase = objMySQLiteOpenHelper.getWritableDatabase();
+        readSqLiteDatabase = objMySQLiteOpenHelper.getReadableDatabase();
+    }
+
+    public long addNewFood(String strFood, String strSource, String strPrice) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_FOOD, strFood);
+        contentValues.put(COLUMN_SOURCE, strSource);
+        contentValues.put(COLUMN_PRICE, strPrice);
+
+
+        return readSqLiteDatabase.insert(FOOD_TABLE, null, contentValues);
+    }
+}
+
---
+++ app/src/main/java/com/pongsakon/pongsakon_restaurant/UserTABLE.java
@@ -0,0 +1,43 @@
+package com.pongsakon.pongsakon_restaurant;
+
+/**
+ * Created by FB Sue Supertiger on 30-Nov-16.
+ */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Admin on 19-Oct-16.
+ */
+public class UserTABLE {
+
+    private MySQLiteOpenHelper objMySQLiteOpenHelper;
+
+    private SQLiteDatabase writeSqLiteDatabase;
+    private SQLiteDatabase readSqLiteDatabase;
+
+    public static final String USER_TABLE = "userTABLE";
+    public static final String COLUMN_ID_USER = "_id";
+    public static final String COLUMN_USER = "User";
+    public static final String COLUMN_PASSWORD = "Password";
+    public static final String COLUMN_NAME = "Name";
+
+
+    public UserTABLE(Context context) {
+        objMySQLiteOpenHelper = new MySQLiteOpenHelper(context);
+        writeSqLiteDatabase = objMySQLiteOpenHelper.getWritableDatabase();
+        readSqLiteDatabase = objMySQLiteOpenHelper.getReadableDatabase();
+    }
+
+
+    public long addNewUser(String strUser, String strPass, String strName) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_USER, strUser);
+        contentValues.put(COLUMN_PASSWORD, strPass);
+        contentValues.put(COLUMN_NAME, strName);
+
+
+        return readSqLiteDatabase.insert(USER_TABLE, null, contentValues);
+    }
+}
